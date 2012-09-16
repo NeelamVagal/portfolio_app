74e846368fd7b1e5f3202c2958f1fc5ba6dcaffe
unknown <Administrator@celab2-14a.comp1-domain.spit.ac.in>
2012-09-16 15:11:19 +0530

    update

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,9 @@
 class Company < ActiveRecord::Base
+ validate :symbol_restrictions
+
+  def symbol_restrictions
+    if symbol.length < 3 or symbol.length >4
+      errors.add :symbol, 'Symbol must be greater than 3 or less than 4'
+    end
+  end
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,11 @@
 class Investment < ActiveRecord::Base
+  validate :quantity_should_be_positive
+  def quantity_should_be_positive
+     if quantity < 0
+       errors.add :quantity, 'should be positive'
+     end
+    if cost < 0
+      errors.add :cost, 'should be positive'
+    end
+  end
 end
